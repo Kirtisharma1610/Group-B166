cbd950d25d366a51661c63f60f48d46aefba6373
Kirtisharma1610 <ks25acf@herts.ac.uk>
2025-12-05 22:56:40 +0000

    Loaded  my dataset

--- my r script assignment group B166.R
+++ my r script assignment group B166.R
@@ -1,1 +1,14 @@
+# libraries
+library(dplyr)
+library(ggplot2)
+library(tibble)
+
+
+#  Load Dataset
+
+df <- read.csv("6- infectious-and-parasitic-diseases-death-rate-who-mdb.csv")
+
+
+names(df)[names(df) == "Age.standardized.deaths.that.are.from.infectious.and.parasitic.diseases.per.100.000.people..in.both.sexes.aged.all.ages"] <- "DeathRate"
+
 

d88a7ed6f5d340e1cad7ac6cb73f6f5bd98f94ec
Kirtisharma1610 <ks25acf@herts.ac.uk>
2025-12-08 01:25:19 +0000

    Change the column name

--- my r script assignment group B166.R
+++ my r script assignment group B166.R
@@ -1,72 +1,32 @@
-# libraries
-library(dplyr)
-library(ggplot2)
-library(tibble)
 
+> library(readr)
+data <- read.csv("6- infectious-and-parasitic-diseases-death-rate-who-mdb.csv")
+> 
+> head(data)
+   Entity Code Year
+1 Albania  ALB 1987
+2 Albania  ALB 1988
+3 Albania  ALB 1989
+4 Albania  ALB 1992
+5 Albania  ALB 1993
+6 Albania  ALB 1994
+  Age.standardized.deaths.that.are.from.infectious.and.parasitic.diseases.per.100.000.people..in.both.sexes.aged.all.ages
+1                                                                                                                13.34700
+2                                                                                                                11.11083
+3                                                                                                                15.65497
+4                                                                                                                 9.98638
+5                                                                                                                13.13065
+6                                                                                                                11.63521
+> 
+> 
+> colnames(data)[4] <- "DeathRate"
+> 
+> head(data)
+   Entity Code Year DeathRate
+1 Albania  ALB 1987  13.34700
+2 Albania  ALB 1988  11.11083
+3 Albania  ALB 1989  15.65497
+4 Albania  ALB 1992   9.98638
+5 Albania  ALB 1993  13.13065
+6 Albania  ALB 1994  11.63521
 
-#  Load Dataset
-
-df <- read.csv("6- infectious-and-parasitic-diseases-death-rate-who-mdb.csv")
-
-
-names(df)[names(df) == "Age.standardized.deaths.that.are.from.infectious.and.parasitic.diseases.per.100.000.people..in.both.sexes.aged.all.ages"] <- "DeathRate"
-
-
-#  Add Income Groups
-
-income_data <- tribble(
-  ~Code, ~IncomeGroup,
-  "AFG", "Low income", 
-  "ALB", "Upper-middle income",
-  "DZA", "Upper-middle income",
-  "AGO", "Lower-middle income",
-  "ARG", "High income",
-  "AUS", "High income",
-  "AUT", "High income",
-  "BGD", "Lower-middle income",
-  "BEL", "High income",
-  "BEN", "Low income",
-  "BFA", "Low income",
-  "BOL", "Lower-middle income",
-  "BRA", "Upper-middle income",
-  "CAN", "High income",
-  "CHN", "Upper-middle income",
-  "COL", "Upper-middle income",
-  "COD", "Low income",
-  "DEU", "High income",
-  "DNK", "High income",
-  "EGY", "Lower-middle income",
-  "ETH", "Low income",
-  "FRA", "High income",
-  "GBR", "High income",
-  "GHA", "Lower-middle income",
-  "IND", "Lower-middle income",
-  "KEN", "Lower-middle income",
-  "LUX", "High income",
-  "MAR", "Lower-middle income",
-  "MEX", "Upper-middle income",
-  "NGA", "Lower-middle income",
-  "NPL", "Low income",
-  "NOR", "High income",
-  "PAK", "Lower-middle income",
-  "RWA", "Low income",
-  "SDN", "Low income",
-  "SWE", "High income",
-  "TZA", "Low income",
-  "UGA", "Low income",
-  "USA", "High income",
-  "ZAF", "Upper-middle income",
-  "ZMB", "Lower-middle income",
-  "ZWE", "Lower-middle income"
-)
-
-# Merge into main dataset
-df <- df %>%
-  left_join(income_data, by = "Code")
-
-# Some datasets cause duplicate income columns → fix
-df <- df %>%
-  mutate(
-    IncomeGroup = coalesce(IncomeGroup, IncomeGroup.x, IncomeGroup.y, IncomeGroup.x.x, IncomeGroup.y.y)
-  ) %>%
-  select(Entity, Code, Year, DeathRate, IncomeGroup)
